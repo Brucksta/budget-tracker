0d019969fb33d7b6a599e16bde268e7e2a808422
Peter Bruckner <peterbruckner08@gmail.com>
2021-07-28 16:14:11 +0930

    updated caching

--- workbox-config.js
+++ workbox-config.js
@@ -3,10 +3,6 @@ module.exports = {
 	globPatterns: [
 		'**/*.{png,html,js,css}'
 	],
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	],
 	swDest: 'public/sw.js',
 	runtimeCaching: [
 		{
@@ -29,5 +25,3 @@ module.exports = {
 
 };
 
-
-//find workbox build caching
